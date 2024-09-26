627788489a5ce99becee1a3363a723d6d0111bd3
Josué Salazaku <josuesalazaku@gmail.com>
2024-09-27 00:53:11 +0200

    Add button text and script file

---
+++ script.js
@@ -0,0 +1,10 @@
+const section = document.getElementById('section')
+const blockedSiteInput = document.getElementById('blocked-site')
+const timeInput = document.getElementById('time')
+const submitBtn = document.getElementById('submit-btn')
+
+
+submitBtn.addEventListener("click", () => {
+    console.log("Button clicked!")
+})
+    
