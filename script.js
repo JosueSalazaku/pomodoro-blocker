af1fef941bc829373c88940b13b6865a86c21347
Josué Salazaku <josuesalazaku@gmail.com>
2024-09-27 14:11:50 +0200

    Capture input data and store as array

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+'use strict'
 const section = document.getElementById('section')
-const blockedSiteInput = document.getElementById('blocked-site')
+const blockedSiteInput = document.getElementById('blocked-sites') 
 const timeInput = document.getElementById('time')
 const submitBtn = document.getElementById('submit-btn')
 
 
+
+function convertData() {
+    const inputData = blockedSiteInput.value; 
+    const timeData = parseInt(timeInput.value);  
+
+    const sitesArray = inputData.split(',').map(site => site.trim());
+
+    console.log("Converted Websites Array:", sitesArray);
+    console.log("Block Time in Minutes:", timeData);
+}
+
 submitBtn.addEventListener("click", () => {
-    console.log("Button clicked!")
+    convertData()
 })
-    
