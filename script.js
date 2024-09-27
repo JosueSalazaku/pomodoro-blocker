2620923f924a84d5af5dd22d2b4493fd726ed69a
Josué Salazaku <josuesalazaku@gmail.com>
2024-09-27 14:19:41 +0200

    Store the data in chrome.storage.sync

--- script.js
+++ script.js
@@ -4,9 +4,8 @@ const blockedSiteInput = document.getElementById('blocked-sites')
 const timeInput = document.getElementById('time')
 const submitBtn = document.getElementById('submit-btn')
 
-
-
 function convertData() {
+    // Retrieve inpute data and store in an Array
     const inputData = blockedSiteInput.value; 
     const timeData = parseInt(timeInput.value);  
 
@@ -14,6 +13,16 @@ function convertData() {
 
     console.log("Converted Websites Array:", sitesArray);
     console.log("Block Time in Minutes:", timeData);
+
+    // Store the data in chrome.storage.sync
+    chrome.storage.sync.set({
+        blockedSites: sitesArray,
+        blockedDuration: timeData,
+        blockStartTime: Date.now()
+    }, function () {
+        console.log("Blocked sites and duration saved to chrome storage:", { sitesArray, timeData });
+        alert(`Sites ${sitesArray} blocked for ${timeData}, Lets Focus`)
+    })
 }
 
 submitBtn.addEventListener("click", () => {
